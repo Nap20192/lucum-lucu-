46d6f334638343735001b76ded01522fe20f2f29
chaos-painter <pytrai@mail.ru>
2024-10-14 20:42:16 +0500

    accordion and clock

---
+++ scripts/forum.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const visibleCharCount = 100;
+
+
+  document.querySelectorAll(".card-text").forEach((cardText) => {
+    const fullText = cardText.innerText;
+
+    if (fullText.length > visibleCharCount) {
+      const visibleText = fullText.slice(0, visibleCharCount);
+      const hiddenText = fullText.slice(visibleCharCount);
+
+      cardText.innerHTML = `
+        ${visibleText}<span class="dots">...</span> <span class="more-text" style="display: none;">${hiddenText}</span>
+      `;
+    }
+  });
+
+
+  document.querySelectorAll(".card-link").forEach((button) => {
+    button.addEventListener("click", function() {
+      const cardBody = button.closest(".card-body");
+      const moreText = cardBody.querySelector(".card-text .more-text");
+      const dots = cardBody.querySelector(".card-text .dots");
+
+      if (moreText.style.display === "none") {
+        moreText.style.display = "inline";
+        dots.style.display = "none"
+        button.innerHTML = "Less...";
+      } else {
+        moreText.style.display = "none";
+        dots.style.display = "inline"
+        button.innerHTML = "More...";
+      }
+    });
+  });
+});
